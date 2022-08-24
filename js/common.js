7e112385b310fb23af154a7f11370539ae016f2e
jeongsehyeon <kuro7529@gmail.com>
2022-08-24 23:23:32 +0900

    ver. 220824-1

--- js/common.js
+++ js/common.js
@@ -17,12 +17,22 @@ $('body').on('click', function(e){
 
 // 화면 사이즈 변환시 nav 초기화
 $(window).resize(function(){ 
-    if (window.innerWidth < 600) { // 다바이스 크기가 600이하일때  
+    if (window.innerWidth < 600) { // 다바이스 크기가 600미만일때  
         $('.my-data-menu').removeAttr('style'); 
     } else { // 다바이스 크기가 600이상일때  
         $('.nav-btn, .m-nav-wrap, .m-nav-dim').removeAttr('style')
     }
 }).resize(); 
+
+$(window).resize(function(){ 
+    if (window.innerWidth < 960) {  // 다바이스 크기가 960미만일때 
+        $(".search input").attr("placeholder", "주소를 검색하세요.");
+    } else {
+        $(".search input").attr("placeholder", "주소를 검색해 원하는 등기를 쉽고 빠르게 열람하세요.");
+    }
+    
+    }).resize(); 
+    출처: https://hongpage.kr/20 [홍페이지 HONGPAGE:티스토리]
 
 // 모바일 메뉴버튼 클릭
 $(document).on('click', '.nav-btn', function () {
